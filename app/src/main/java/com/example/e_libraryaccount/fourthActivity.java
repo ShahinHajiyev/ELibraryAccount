a94cc4734a1e8b61a7c31fe2bb60ba90ee175375
ShahinHajiyev <hshahin.t401@code.edu.az>
2021-12-05 13:01:24 +0100

    Seekbar is added to the fourth Activity to modoify the brightness during reading

--- app/src/main/java/com/example/e_libraryaccount/fourthActivity.java
+++ app/src/main/java/com/example/e_libraryaccount/fourthActivity.java
@@ -2,20 +2,75 @@ package com.example.e_libraryaccount;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
 
 import com.github.barteksc.pdfviewer.PDFView;
 
 public class fourthActivity extends AppCompatActivity {
 
     PDFView pdfView;
+    CheckBox checkBox;
+    SeekBar seekBar;
+
+    private int color;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fourth);
 
+        askPermission(this);
+
         String contentOfBooks = getIntent().getStringExtra(thirdActivity.CONTENT);
+
+        checkBox = findViewById(R.id.checkBox);
+        seekBar = findViewById(R.id.seekBar);
+
+        seekBar.setMax(255);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Settings.System.putInt(getContentResolver(),Settings.System.SCREEN_BRIGHTNESS, progress);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    seekBar.setVisibility(View.VISIBLE);
+                }
+                else {
+                    seekBar.setVisibility(View.INVISIBLE);
+
+                }
+            }
+        });
 
         switch (contentOfBooks) {
             case thirdActivity.PYTHON:
@@ -37,5 +92,20 @@ public class fourthActivity extends AppCompatActivity {
     public void openContent(String name){
         pdfView = findViewById(R.id.pdfView);
         pdfView.fromAsset(name).load();
+
     }
+
+    public void askPermission(Context context){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (Settings.System.canWrite(context));
+            else{
+                Intent i = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                context.startActivity(i);
+            }
+
+
+        }
+    }
+
+
 }
